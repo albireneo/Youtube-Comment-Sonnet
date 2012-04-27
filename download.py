e1c208d53b5c65e469e493d05a5495546551d14b
Laura Macaddino <macaddino@uchicago.edu>
2012-04-27 08:32:42 -0500

    comment_collector and example sonnet.txt file

--- download.py
+++ download.py
@@ -4,6 +4,10 @@ import urllib2
 from os.path import basename
 from urlparse import urlsplit
 from bs4 import BeautifulSoup
+import re
+import random
+
+#global variables
 
 
 #given a website URL, saves the source code as sauce.htm 
@@ -22,7 +26,15 @@ def url_parser():
     youtube = "http://www.youtube.com"
     site = open("sauce.htm")
     data = site.read()
+ 
+    #Pick one of the first ten related videoes based on random number
+    videoRand = random.randint(0,9)
+
     offset09 = data.find("related-video")
+    i = 0
+    while i < videoRand:
+        offset09 = offset09+13 + data[offset09+13:].find("related-video")
+        i = i + 1
 
     #some videos have posted video responses under "related-videos"
     #there are only two posted responses visible per page, and this 
@@ -43,3 +55,64 @@ def url_parser():
     final = youtube+end
     return final
 	
+#comment_collector: 
+
+def comment_collector():
+    site = BeautifulSoup(open("sauce.htm"))
+
+    comArray = [None]*10
+    #put all comments in comArray
+    i = 0
+    while i < 10: 	
+        for string in site.find_all("div", "comment-text", limit = i+1):
+            comArray[i] = repr(string)
+        i = i + 1 
+
+    #if comments are disabled
+    if comArray[0] == None:
+        nextPage = url_parser()
+        website_retriever(nextPage)
+        return 0
+
+    #clean up all comments
+    i = 0 
+    while i < 10:
+        comArray[i] = comArray[i].partition('\n<p>')[2]
+        comArray[i] = comArray[i].partition('</p>\n</div>')[0]
+        #account for line indents
+        comArray[i] = re.sub('</p>\n<p>', " ", comArray[i])
+        comArray[i] = re.sub('\xef\xbb\xbf', "", comArray[i])
+        comArray[i] = re.sub('&quot;', '"', comArray[i])
+        comArray[i] = re.sub('&gt;', '>', comArray[i])
+        comArray[i] = re.sub('&lt;', '<', comArray[i])  
+	#account for video offset links
+        comArray[i] = re.sub('<a href="#" onclick="yt\.www\.watch\.player\.seekTo\(\S+\);return false;">\S+</a>', "", comArray[i])
+	i = i + 1
+
+    #send comments to be parsed and possibly written into sonnet
+    i = 0
+    while i < 10:
+        if comment_parser(comArray[i]) != "NO_PARSE":
+	    sonnet = open('sonnet.txt', 'a')	
+            sonnet.seek(0, 2) #Go the the 0th byte before the end of file
+            sonnet.write(comArray[i]+'\n')
+	    sonnet.close()
+        i = i + 1
+
+    nextPage = url_parser()
+    website_retriever(nextPage)
+    return 0
+
+
+def comment_parser(comment):
+    if comment == None:
+        return "NO_PARSE";
+    else:
+        return comment;
+
+	 	
+
+
+
+
+  		
